df3aed0f4cc0029bc4b9f774fa7cab224eeeffe8
Rahul D <rahul_d@epam.com>
2024-08-03 02:05:48 +0530

    First commit

--- src/main/java/com/epam/rd/autotasks/Battleship8x8.java
+++ src/main/java/com/epam/rd/autotasks/Battleship8x8.java
@@ -1,18 +1,96 @@
 package com.epam.rd.autotasks;
 
+import java.util.List;
+
 public class Battleship8x8 {
     private final long ships;
-    private long shots = 0L;
+    String map;
+    char []resultMap;
+
+    String getMap(long ship)
+    {
+        StringBuilder str= new StringBuilder();
+
+        if(ship < 0)
+        {
+            ship = ~(-1*ship) + 1;
+        }
+
+        for(int i=0;i<64;i++)
+        {
+            int x = (int)(ship & 1);
+            str.append(x);
+            ship>>=1;
+        }
+        str.reverse();
+        return str.toString();
+    }
 
     public Battleship8x8(final long ships) {
         this.ships = ships;
+        this.map = getMap(ships);
+        this.resultMap = new char[64];
+
+        for(int i=0;i<64;i++)
+        {
+            if(map.charAt(i) == '1') resultMap[i] = '1';
+            else resultMap[i] = '0';
+        }
     }
 
     public boolean shoot(String shot) {
-        throw new UnsupportedOperationException();
+        int decodeChar = (int)(shot.charAt(0) - 'A');
+        int decodeNum = (int)(shot.charAt(1) - '1');
+        int index = (decodeChar) + 8*decodeNum;
+
+        if(map.charAt(index) == '1')
+        {
+            resultMap[index] = '☒';
+            return true;
+        }
+        else
+        {
+            resultMap[index] = '×';
+        }
+        return false;
     }
 
     public String state() {
-        throw new UnsupportedOperationException();
+
+        StringBuilder mapState = new StringBuilder();
+        for(int i=0;i<64;i++)
+        {
+            if(resultMap[i] == '1')
+            {
+                resultMap[i] = '☐';
+            }
+            else if(resultMap[i] == '0')
+            {
+                resultMap[i] = '.';
+            }
+        }
+
+        for(int i=0;i<64;i++)
+        {
+            if(i != 0 && i % 8 == 0) mapState.append("\n");
+            mapState.append(resultMap[i]);
+        }
+
+        return mapState.toString();
+    }
+
+    public static void main(String[] args) {
+        List<String> shots = List.of("A3", "D7", "A5", "F3", "H8", "D3", "B3", "G8", "C3");
+        long map = 5783259913403860192L;
+        Battleship8x8 obj  = new Battleship8x8(map);
+
+        for(String x : shots)
+        {
+            obj.shoot(x);
+        }
+
+        String res = obj.state();
+        System.out.println(res);
+
     }
 }

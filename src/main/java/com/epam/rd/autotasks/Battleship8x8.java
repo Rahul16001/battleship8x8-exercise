766a5d524f0dc7e23a92f6696534b47c2def5e44
Rahul D <rahul_d@epam.com>
2024-08-04 02:17:03 +0530

    Used bitwise operations to optimize the code

--- src/main/java/com/epam/rd/autotasks/Battleship8x8.java
+++ src/main/java/com/epam/rd/autotasks/Battleship8x8.java
@@ -15,11 +15,11 @@ public class Battleship8x8 {
         int row = shot.charAt(1) - '1';
         int index = (col) + 8*row;
 
-        long shotBit = 1L << (63 - index);
-        shots|=shotBit;
-        return (ships & shotBit) != 0L;
-
-    }
+        long shotBit = 1L << (63 - index); //Here we are shifting bits to left....
+        shots|=shotBit; // here we are storing shot at a particular index in shots
+        return (ships & shotBit) != 0L; // if there is a ship at the index where we shot return true else false
+                                       //here we are checking if value from ships & shotBit is not 0L then there must be
+    }                                  //a ship at that location which is contra-positive of equal to some positive value
 
     public String state() {
 
@@ -27,12 +27,12 @@ public class Battleship8x8 {
 
         for(int i = 0 ;i < 64; i++)
         {
-            if(i > 0 && i % 8 == 0) res.append("\n");
+            if(i > 0 && i % 8 == 0) res.append("\n");// after every 8 bits we are appending a new line
 
-            long index = 1L << (63- i);
+            long index = 1L << (63- i); // this line gets bit from left side
 
-            boolean isShot = (shots & index) != 0L;
-            boolean isShip = (ships & index) != 0L;
+            boolean isShot = (shots & index) != 0L; // this line checks if we have shot at the particular index
+            boolean isShip = (ships & index) != 0L;// this line checks if there is a ship at the particular index
             System.out.println(isShot +" "+isShip);
 
             if(isShot)
